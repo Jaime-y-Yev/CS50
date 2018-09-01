3ccb34450578d30ac5778f767196098b8f45cb79
Jaime Salazar <jsalaz1989@gmail.com>
2018-09-01 16:25:37 +0000

    Renamed folder so had to re-add mario.c and credit.c

---
+++ problem set 1/credit.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <math.h>
+
+string getCardType();
+int getCardSum();
+
+
+int main(void)
+{
+    // Requess card number from user
+    printf("Number: ");
+    long long cardNum = get_long_long();
+
+    int cardSum = getCardSum(cardNum);
+    //printf("cardSum = %i", cardSum);
+
+    if (cardSum % 10 == 0)
+        printf("%s\n", getCardType(cardNum));
+    else
+        printf("INVALID\n");
+
+    return 0;
+}
+
+// Return card company name based on card number input
+string getCardType(long long cardNum)
+{
+    if (cardNum >= 5100000000000000 && cardNum < 5600000000000000)
+        return "MASTERCARD\n";
+
+    else if ((cardNum >= 4000000000000 && cardNum < 5000000000000) || (cardNum >= 4000000000000000 && cardNum < 5000000000000000))
+        return "VISA\n";
+
+    else if ((cardNum >= 340000000000000 && cardNum < 350000000000000) || (cardNum >= 370000000000000 && cardNum < 380000000000000))
+        return "AMEX\n";
+    else
+        return "INVALID\n";
+}
+
+// Return a processed total based on the card number input
+int getCardSum(long long cardNum)
+{
+    int sumOdd = 0;
+    int sumEven = 0;
+    int x1, x2;
+
+    // Odd positions summed up in sumOdd
+    for (int i = 1; i <= 16; i = i + 2)
+    {
+        long long cardNumNew = cardNum / pow(10,16-i);
+        long long cardNumNew1 = cardNumNew * 10;
+        long long cardNumNew2 = cardNum / pow(10,15-i);
+        long long y = cardNumNew2 - cardNumNew1;
+
+        sumOdd = sumOdd + y;
+    }
+
+    // Even positions summed up according to Luhn algorithm
+    for (int i = 0; i <= 15; i = i + 2)
+    {
+        // Even positions first multiplied by 2
+        long long cardNumNew = cardNum / pow(10,16-i);
+        long long cardNumNew1 = cardNumNew * 10;
+        long long cardNumNew2 = cardNum / pow(10,15-i);
+        long long x = 2 * (cardNumNew2 - cardNumNew1);
+
+        // Any resulting double digits are split into two digitsseparated into their two constituent digits
+        if (x >= 10)
+        {
+            x1 = x / 10;
+            x2 = x - 10;
+            x = x1 + x2;
+        }
+
+        sumEven = sumEven + x;
+    }
+
+    return sumOdd + sumEven;
+}
---
+++ problem set 1/mario.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    // Get the number of blocks from the user
+    int height = -1;
+    while (height < 0 || height > 23)
+    {
+        printf("Height (0-23): ");
+        height = get_int();
+    }
+
+    // Large loop for each row, nested loops for characters within the row
+    int i, j;
+    for (i = height; i >= 1; i--)
+    {
+        for (j = i - 1; j > 0; j--)         // spaces before the hashes before the repeating block
+            printf(" ");
+
+        for (j = 0; j < height - i; j++)    // hashes before the repeating block
+            printf("#");
+
+        printf("#  #");                     // central, repeating block
+
+        for (j = 0; j < height - i; j++)    // hashes after the repeating block
+            printf("#");
+
+        printf("\n");                       // newline before starting next tow
+    }
+
+    return 0;
+}
