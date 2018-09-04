26a30b8eb84d9485dc70115306657a514d21733e
Jaime Salazar <jsalaz1989@gmail.com>
2018-09-04 15:20:35 +0000

    Added comments to my functions in helpers.c

--- problem set 3/music/helpers.c
+++ problem set 3/music/helpers.c
@@ -7,8 +7,8 @@
 
 #include "helpers.h"
 // Cannot include these declarations in helpers.h because check50 uses its own version (source: https://cs50.stackexchange.com/questions/27014/pset3-find-check50-frown-helpers-c-compiles-expected-exit-code-0-not-1)
-double getSemitonesBrute(string note);
-double getSemitones(string note);
+double getSemitonesBrute(string note);  // Determines the number of semitones between note and A4 by looping through all possible hardcoded notes
+double getSemitones(string note);       // Determines the number of semitones between note and A4 by looping only through all the letters and then adjusting for the octave
 
 
 // Converts a fraction formatted as X/Y to eighths
@@ -17,21 +17,15 @@ int duration(string fraction)
     double top = atoi(&fraction[0]);
     double bottom = atoi(&fraction[2]);
 
-    //printf("top = %f \n", top);
-    //printf("bottom = %f \n", bottom);
-
     double eighths = 8 * (top / bottom);
-    //printf("eighths = %f \n", eighths);
-
     return eighths;
 }
 
-// Calculates frequency (in Hz) of a note (ej. C5, G#2, Ab3)
+// Calculates frequency (in Hz) of a note (eg. C5, G#2, Ab3)
 int frequency(string note)
 {
     double semitones = getSemitones(note);
     //double semitones = getSemitonesBrute(note);
-    //printf("semitones = %f \n", semitones);
 
     // The frequency f = 2^(n/12) × 440. where n = number of semitones
     double exponent = -semitones/12;            // I suppose the way the semitones are calculated requires that this exponent be negative (?)
@@ -48,9 +42,10 @@ bool is_rest(string s)
         return false;
 }
 
-
+// Determines the number of semitones between note and A4 by looping through all possible hardcoded notes
 double getSemitonesBrute(string note)
 {
+    // All possible notes between C0 and B8
     string notes[] = {"C0", "C#0", "D0", "D#0", "E0", "F0", "F#0", "G0", "G#0", "A0", "A#0", "B0",
                       "C1", "C#1", "D1", "D#1", "E1", "F1", "F#1", "G1", "G#1", "A1", "A#1", "B1",
                       "C2", "C#2", "D2", "D#2", "E2", "F2", "F#2", "G2", "G#2", "A2", "A#2", "B2",
@@ -61,10 +56,12 @@ double getSemitonesBrute(string note)
                       "C7", "C#7", "D7", "D#7", "E7", "F7", "F#7", "G7", "G#7", "A7", "A#7", "B7",
                       "C8", "C#8", "D8", "D#8", "E8", "F8", "F#8", "G8", "G#8", "A8", "A#8", "B8"};
 
-    int indexLetter = 0;
-    int indexA4 = 57;
+    // Loop through all possible notes until matching with note, but first ensure any flats are converted to their equivalent sharps
+    int indexA4 = 57;       // A4 always appears at notes[57]
+    int indexNote = 0;      // the position in notes[] at which note appears
     for (int i = 0; i < 108; i++)
     {
+        // Convert any flats to their equivalent sharps (eg. Gb3 = A#3)
         if (note[1] == 'b')
         {
             note[1] = '#';
@@ -81,45 +78,46 @@ double getSemitonesBrute(string note)
                 note[0] = 'C';
         }
 
+        // Find the index at which note appears in notes[]
         if (strcmp(note, notes[i]) == 0)
         {
-            indexLetter = i;
+            indexNote = i;
             break;
         }
     }
-    //printf("indexA4 = %i \n", indexA4);
-    //printf("indexletter = %i \n", indexLetter);
-    //printf("indexA4 - indexLetter = %i \n", indexA4 - indexLetter);
 
-    double semitones = indexA4 - indexLetter;
+    double semitones = indexA4 - indexNote;     // the number of semitones is the difference in positions within notes[]
     return semitones;
 }
 
-
+// Determines the number of semitones between note and A4 by looping only through all the letters and then adjusting for the octave
 double getSemitones(string note)
 {
-    int octave;
-    char letter[3] = {'\0', '\0', '\0'};
-    if (strlen(note) == 2)
-    {
-        letter[0] = note[0];
-        octave = atoi(&note[1]);
+    char letter[3] = {'\0', '\0', '\0'};    // eg. if note="B2" then letter="B", if note="C#6" then letter="C#""
+    int octave;                             // eg. if D#4 then octave=4, if Gb3 then octave=3
+    if (strlen(note) == 2)                          // eg. note="B2"
+    {                                               //
+        letter[0] = note[0];                        //     letter="B"
+        octave = atoi(&note[1]);                    //     octave=2
     }
-    else if (strlen(note) == 3)
-    {
-        letter[0] = note[0];
-        letter[1] = note[1];
-        octave = atoi(&note[2]);
+    else if (strlen(note) == 3)                     // eg. note="C#6"
+    {                                               //
+        letter[0] = note[0];                        //     letter="C#"
+        letter[1] = note[1];                        //
+        octave = atoi(&note[2]);                    //     octave=6
     }
 
-    string notes[] = {"C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"};
+    // All possible letters between C and B, ignoring the octave
+    string letters[] = {"C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"};
 
-    int indexA = 9;
-    int indexletter = 0;
+    // Loop through all the letters until matching with letter, but first ensure any flats are converted to their equivalent sharps
+    int indexA = 9;         // A always appears at letters[9]
+    int indexletter = 0;    // the position in notes[] at which letter appears
     for (int i = 0; i < 12; i++)
     {
         if (letter[1] == 'b')
         {
+            // Convert any flats to their equivalent sharps (eg. Gb3 = A#3)
             letter[1] = '#';
 
             if (letter[0] == 'B')
@@ -134,7 +132,8 @@ double getSemitones(string note)
                 letter[0] = 'C';
         }
 
-        if (strcmp(letter, notes[i]) == 0)
+        // Find the index at which letter appears in letters[]
+        if (strcmp(letter, letters[i]) == 0)
             indexletter = i;
     }
 
