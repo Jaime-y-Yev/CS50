cfece701669e585b94cfe134507b9ee0807a9471
Jaime Salazar <jsalaz1989@gmail.com>
2018-09-08 15:58:35 +0000

    Minor mod to caesar.py

--- problem set 6/sentimental/caesar.py
+++ problem set 6/sentimental/caesar.py
@@ -19,7 +19,8 @@ except ValueError:
 plainText = input("plaintext: ")
 
 # Only encrypt A-Z and a-z characters
-encryptableLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+encryptableLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+                      'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 # Rotate every character a value of key up the alphabet, ignores special characters
 cipherText = []
